76876c0715896c27085f9553d305937256014f08
5riman <cheetisriman@gmail.com>
2025-06-11 23:02:46 +0530

    Add project files.

---
+++ MVCExample1/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCExample1.Controllers
+{
+    public class HomeController:Controller
+    {
+        public string Hello()
+        {
+            return "My name is sriman";
+
+        }
+
+        public string Index()
+        {
+            return "Hello iam Index";
+        }
+    }
+}
---
+++ MVCExample1/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCExample1.Controllers
+{
+    public class TestController:Controller
+    {
+        public ViewResult register()
+        {
+            return View();
+
+        }
+      public ViewResult Login()
+        {
+            return View();
+        }
+
+    }
+}
