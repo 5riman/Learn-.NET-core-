b5c3a2a834a9ff8f8e5667e3a045f6b9feb8e7f8
5riman <72627520+5riman@users.noreply.github.com>
2025-06-25 23:20:35 +0530

    added controller and views

---
+++ ASP.Net_Core_Middleware/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.Net_Core_Middleware.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
