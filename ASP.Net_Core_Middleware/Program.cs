da62dc6f13d8ee03b1ff41e51fd76735d3b786b1
5riman <72627520+5riman@users.noreply.github.com>
2025-06-25 23:18:54 +0530

    Update Program.cs
    added conventional based routing to program.cs

--- ASP.Net_Core_Middleware/Program.cs
+++ ASP.Net_Core_Middleware/Program.cs
@@ -1,18 +1,26 @@
+using Microsoft.AspNetCore.Mvc.Diagnostics;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
-app.Use(async (context, next) =>
-{
-    await context.Response.WriteAsync("Hello from middleware!\n");
-    await next(context);
-});
+//app.Use(async (context, next) =>
+//{
+//    await context.Response.WriteAsync("Hello from middleware!\n");
+//    await next(context);
+//});
 
-app.Run(async (context) =>
-{
-    await context.Response.WriteAsync("Hello this is Sriman");
-});
+//app.Run(async (context) =>
+//{
+//    await context.Response.WriteAsync("Hello this is Sriman");
+//});
 
 
 //app.MapGet("/", () => "Hello World!");
+
+app.MapControllerRoute(
+    name:"default",
+    pattern:"{controller=Home}/{action=Index}/{id?}"
+    );
 
 app.Run();
