4af72005dbdf2b76a52191ff9660b58baf4d286d
5riman <72627520+5riman@users.noreply.github.com>
2025-07-08 22:10:00 +0530

    used attribute based routing in HomeController.cs

--- ASP.Net_Core_Middleware/Controllers/HomeController.cs
+++ ASP.Net_Core_Middleware/Controllers/HomeController.cs
@@ -2,8 +2,12 @@
 
 namespace ASP.Net_Core_Middleware.Controllers
 {
+[Route("[controller]")]
     public class HomeController : Controller
     {
+      [Route("")]
+      [Route("[action]")]
+      [Route("~/")]
         public IActionResult Index()
         {
             return View();
